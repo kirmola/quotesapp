e71e87ea61bf6a438e5cb3a300a5a1221d602942
Aman Rawat <ar.rawataman@gmail.com>
2023-11-24 11:16:54 +0530

    updated quote url slug

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("topics/", views.topics, name="topics"),
     path("authors/", views.authors, name="authors"),
-    path("<slug:quote_url>/", views.quote, name="Quote_detail"),
+    path("quote/<slug:quote_url>/", views.quote, name="Quote_detail"),
     path("topics/<slug:topic_name>/", views.topics, name="Topic_detail"),
     path("authors/<slug:author_name>/", views.authors, name="Author_detail"),
 ]
