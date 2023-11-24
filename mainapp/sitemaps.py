887edb4ca0aaf8dfed9e400e7dfaa5a80a2186cc
Aman Rawat <ar.rawataman@gmail.com>
2023-11-24 17:45:55 +0530

    sitemap.py: limit 50000 urls & included i18n urls

--- mainapp/sitemaps.py
+++ mainapp/sitemaps.py
@@ -4,15 +4,27 @@ from django.urls import reverse
 
 class QuoteSitemap(Sitemap):
     
+    i18n = True
+    
+    limit = 50000
+
     def items(self):
         return Quote.objects.all()
 
 class AuthorSitemap(Sitemap):
     
+    i18n = True
+    
+    limit = 50000
+
     def items(self):
         return Author.objects.all()
 
 class TopicSitemap(Sitemap):
+
+    i18n = True
+    
+    limit = 50000
 
     def items(self):
         return Topic.objects.all()
