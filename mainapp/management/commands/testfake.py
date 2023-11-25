2b2e73c1b1e211d86ca8560a71feaa3ecbab9f84
Aman Rawat <ar.rawataman@gmail.com>
2023-11-25 13:35:27 +0530

    new mgmt. command for putting fake data in DB

---
+++ mainapp/management/commands/testfake.py
@@ -0,0 +1,21 @@
+from faker import Faker
+from mainapp.models import *
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+
+        for _ in range(500):
+            # Create or retrieve an Author instance
+            author_name = fake.name()
+            author_instance, _ = Author.objects.get_or_create(author=author_name)
+
+            # Create or retrieve a Topic instance
+            topic_name = fake.word()
+            topic_instance, _ = Topic.objects.get_or_create(topic=topic_name)
+
+            # Create a Quote instance
+            quote_text = fake.sentence(nb_words=30)
+            quote_instance = Quote.objects.create(quote=quote_text, author=author_instance, topics=topic_instance)
