c2adc7a308f7f78b54a0ad26c58a59c25116f0a0
Aman Rawat <ar.rawataman@gmail.com>
2023-11-30 21:28:20 +0530

    now generating thumbnails too.

--- mainapp/management/commands/imager.py
+++ mainapp/management/commands/imager.py
@@ -28,6 +28,10 @@ class Command(BaseCommand):
             for imagefile in imagefiles_in_dir:
                 bg_images_list.append(f"{bg_image_path}{imagefile}")
 
+        thumbnail_size = (96,54)
+        image_save_location = f"{settings.MEDIA_ROOT}"
+        # thumbnail_save_location = f"{settings.MEDIA_ROOT}/t/"
+
 
         for quote, filename in zip(quotes, filenames):
             imgobj = [Image.open(i) for i in bg_images_list]
@@ -37,16 +41,34 @@ class Command(BaseCommand):
                 imageDrawing = ImageDraw.Draw(each[1])
                 current_y_position = 300 if 160 >= len(quote) >= 40 else 225
                 text_to_write = wrap(quote, width=40)
+                
+                
                 for each_piece in text_to_write:
                     imageDrawing.text(font=font, text=each_piece, xy=(
                         250, current_y_position), stroke_fill="black", stroke_width=5,fill="white")
                     current_y_position += 100
-                    image_name = f"{filename}-{each[0]+1}.png"
-                save_location = f"{settings.MEDIA_ROOT}"
-                if exists(f"{save_location}/{image_name}"):
+
+
+                image_name = f"{filename}-{each[0]+1}.png"
+                thumbnail_name = f"{filename}-{each[0]+1}-thumb.png"
+                
+                
+                
+                if exists(f"{image_save_location}/{image_name}"):
                     self.stdout.write("Image is already there. No need to save.")
                 else:
-                    each[1].save(f"{save_location}/{image_name}")
+
+                    # Image is being saved at this point
+                    each[1].save(f"{image_save_location}/{image_name}")
+
+
+                    # Thumbnail is being saved at this point
+                    # Just remove image_save_location and put thumbnail_save_location below if you want thumbnails to be saved in new location
+                    
+                    each[1].thumbnail(thumbnail_size)
+                    each[1].save(f"{image_save_location}/{thumbnail_name}")
+                    
+                    
                     self.stdout.write(f"'{image_name}' has been saved successfully.")
                 
                 
